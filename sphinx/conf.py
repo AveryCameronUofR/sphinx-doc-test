3a2a8293cb836d3840fc7022f2e88d781deca21f
Avery Cameron <35551198+AveryCameronUofR@users.noreply.github.com>
2020-08-21 09:42:11 -0400

    Page generation

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -10,14 +10,14 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../'))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'Sphinx Documentation Example'
+project = 'Sphinx Doc Test'
 copyright = '2020, Avery Cameron'
 author = 'Avery Cameron'
 
@@ -30,11 +30,16 @@ release = '0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['sphinx.ext.autodoc',
+    'sphinx.ext.githubpages']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
+
+source_suffix = '.rst'
+master_doc = 'index'
+pygments_style = 'sphinx'
+todo_include_todos = False
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
