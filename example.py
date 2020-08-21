f1e099c089afd40f5ef3a5fd5da86d6b8ce81b81
Avery Cameron <35551198+AveryCameronUofR@users.noreply.github.com>
2020-08-21 09:01:35 -0400

    Create functions for documentation

---
+++ example.py
@@ -0,0 +1,19 @@
+def func1(arg1, arg2):
+    """
+    This function takes two arguments, sets the first plus equal to the second, then returns the new first argument.
+
+    :param arg1: Some value
+    :param arg2: another value
+
+    :return: arg1
+    """
+    arg1 += arg2
+    return arg1
+
+def func2():
+    """
+    Function with no arguments, returns None
+
+    :return: None
+    """
+    return None
